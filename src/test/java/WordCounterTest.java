a29c65d1376df965af6b8e26637433cba78d6f08
l-cruz <luciaacruztoste@gmail.com>
2025-02-26 16:23:24 +0000

    test improved

--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -3,9 +3,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WordCounterTest {
+
     @Test
-    public void testUniqueWords() {
-        WordCounter counter = new WordCounter();
-        assertEquals(3, WordCounter.countUniqueWords("hello world hello"));
+    void testPhraseWithRepeatedWords() {
+        String phrase = "Hola mundo hola mundo";
+        int result = WordCounter.countUniqueWords(phrase);
+        assertEquals(2, result);
+    }
+
+    @Test
+    void testEmptyPhrase() {
+        String phrase = " ";
+        int result = WordCounter.countUniqueWords(phrase);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void testPhraseWithPunctuation() {
+        String phrase = "¡Hola, mundo! Hola. Mundo?";
+        int result = WordCounter.countUniqueWords(phrase);
+        assertEquals(2, result);
     }
 }
+
