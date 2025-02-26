fa9a31a7fcbdb4b3168bbd837516632e41e5ce66
carlota <a@a.com>
2025-02-26 16:52:31 +0000

    Add new class SalesReport

---
+++ src/main/java/es/ulpgc/dacd/SalesReport.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+package es.ulpgc.dacd;
+
+public class SalesReport {
+    public void generateReport(String[] salesData) {
+        HashMap<String, Integer> sales = new HashMap<>();
+        for (String sale : salesData) {
+            String[] parts = sale.split(",");
+            String product = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+            if (sales.containsKey(product)) {
+                sales.put(product, sales.get(product) + amount);
+            } else {
+                sales.put(product, amount);
+            }
+        }
+        for (String product : sales.keySet()) {
+            System.out.println(product + ": " + sales.get(product));
+        }
+    }
+}
